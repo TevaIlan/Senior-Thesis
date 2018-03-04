fixed minor error in y calculation but it doesn't appear to have made any real difference

aperturephotometry.py
@@ -49,9 +49,9 @@ for i in range(0,len(catalog)):
 		S150=maps.aperture_photometry(instamp=cutout150,aperture_radius=1.5*np.pi/10800,annulus_width=1.4*np.pi/10800)
 		S150s.append(S150)
 		x90=((6.62607004*10**(-34)*90*10**9)/(1.38064852*10**(-23)*2.7255))
-		a90=2.7255*x90*(np.cosh(x90/2.)/np.sinh(x90/2.))-4
+		a90=2.7255*(x90*(np.cosh(x90/2.)/np.sinh(x90/2.))-4)
 		x150=((6.62607004*10**(-34)*150*10**9)/(1.38064852*10**(-23)*2.7255))
-		a150=2.7255*x150*(np.cosh(x150/2.)/np.sinh(x150/2.))-4
+		a150=2.7255*(x150*(np.cosh(x150/2.)/np.sinh(x150/2.))-4)
 		Ycalculated=(S90-S150)/(a90-a150)
 		CalculatedY.append(Ycalculated)
 		YMeasured=catalog[i][11]
@@ -99,4 +99,4 @@ plt.ylim(ymin,ymax)
 plt.xlabel('Measured Compton Y')
 plt.ylabel('Calculated Compton Y')
 plt.savefig("Calculated vs Measured Y")
-print(np.mean(CalculatedY), np.std(CalculatedY)/np.sqrt(N)) #prints (3.613133052108955e-11, 4.750134609152584e-12)
+print(np.mean(CalculatedY), np.std(CalculatedY)/np.sqrt(N)) #prints (3.613133052108954e-11, 4.750134609152583e-12)